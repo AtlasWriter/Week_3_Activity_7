48f30fb33d85ee123ccdc6b18eae15d47cc70ae8
Daniel Rumfelt <76234995+GCUStudent-DanielRumfelt@users.noreply.github.com>
2022-07-24 23:08:08 -0400

    Add files via upload

---
+++ Week_3_Activity_7/Week_3_Activity_7/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Week_3_Activity_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.termsTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.equalsPi = new System.Windows.Forms.Label();
+            this.guiDisplayTerms = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(38, 88);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(3);
+            this.label1.Size = new System.Drawing.Size(163, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter # of Terms:";
+            // 
+            // termsTextBox
+            // 
+            this.termsTextBox.Location = new System.Drawing.Point(216, 92);
+            this.termsTextBox.Name = "termsTextBox";
+            this.termsTextBox.Size = new System.Drawing.Size(138, 22);
+            this.termsTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(42, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 65);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 252);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(3);
+            this.label2.Size = new System.Drawing.Size(259, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Approximate value of PI after";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 315);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(3);
+            this.label3.Size = new System.Drawing.Size(26, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "=";
+            // 
+            // equalsPi
+            // 
+            this.equalsPi.AutoSize = true;
+            this.equalsPi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.equalsPi.Location = new System.Drawing.Point(51, 315);
+            this.equalsPi.Name = "equalsPi";
+            this.equalsPi.Size = new System.Drawing.Size(0, 20);
+            this.equalsPi.TabIndex = 5;
+            // 
+            // guiDisplayTerms
+            // 
+            this.guiDisplayTerms.AutoSize = true;
+            this.guiDisplayTerms.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guiDisplayTerms.Location = new System.Drawing.Point(302, 254);
+            this.guiDisplayTerms.Name = "guiDisplayTerms";
+            this.guiDisplayTerms.Padding = new System.Windows.Forms.Padding(2);
+            this.guiDisplayTerms.Size = new System.Drawing.Size(4, 24);
+            this.guiDisplayTerms.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(364, 252);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(3);
+            this.label4.Size = new System.Drawing.Size(63, 26);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "terms";
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.resetButton.Location = new System.Drawing.Point(175, 158);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(111, 65);
+            this.resetButton.TabIndex = 8;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.closeButton.Location = new System.Drawing.Point(306, 158);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(111, 65);
+            this.closeButton.TabIndex = 9;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(450, 409);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.guiDisplayTerms);
+            this.Controls.Add(this.equalsPi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.termsTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Approximate Pi";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox termsTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label equalsPi;
+        private System.Windows.Forms.Label guiDisplayTerms;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
+
---
+++ Week_3_Activity_7/Week_3_Activity_7/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week_3_Activity_7
+//  Write a Windows Forms Application that prompts the user for a number of
+//  terms and CALCULATES the value of the sequence for the specified number of terms;
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Once the user opens the program and enters terms then press the calculate button.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Build a varable to sore user input from textbox
+            double termsInput;
+            // Once user presses caluclates button the data is pased as a string and converts it
+            // to intergers. TryParse to create a user friendly exception. 
+            if(double.TryParse(termsTextBox.Text, out termsInput))
+                // Pass user input terms to disply amount entered to label.
+                guiDisplayTerms.Text = termsInput.ToString();
+                // For loop to multiple by the terms user entered.
+                for(int i = 0; i < termsInput; i++)
+                {
+                // Logic caculations algorithm
+                double displayPi = i * 4.177271127019824e-4;
+                // Display calculations from calclations algorithm.
+                equalsPi.Text = displayPi.ToString("#.##############"); 
+            }
+        }
+        // Rest Fields Button
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            // Clear text box
+            termsTextBox.Text = "";
+            // Focus cruser.
+            termsTextBox.Focus();
+        }
+        // Close program button
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            // Closes form.
+            this.Close();
+        }
+    }
+}
